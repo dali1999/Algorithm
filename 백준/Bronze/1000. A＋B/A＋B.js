62d5f674a4b2865f880d7b6e38bc767ec69787d1
dali <dali1999@naver.com>
2024-01-20 01:32:35 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,10 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+let [a, b] = input;
+
+console.log(a + b);
