58741cdac78b2b1c0434823fc82bb7000645ed68
dali <dali1999@naver.com>
2024-01-25 22:51:34 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 200 ms, Memory: 10556 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
@@ -0,0 +1,64 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.split(" "));
+
+const whiteBoard = [
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+];
+
+const blackBoard = [
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+];
+
+const NM = input.shift();
+const N = Number(NM[0]); // 10
+const M = Number(NM[1]); // 13
+
+// 문자열 끝 줄바꿈 문자 제거
+for (let i = 0; i < N; i++) {
+  input[i] = input[i].join("").replace(/\s/g, "");
+}
+let answer = 64;
+
+// input 범위 안에서의 비교
+for (let i = 0; i <= N - 8; i++) {
+  for (let j = 0; j <= M - 8; j++) {
+    check(j, i);
+  }
+}
+
+// 8*8 체스판 범위 안에서의 비교
+function check(x, y) {
+  let checkWhite = 0;
+  let checkBlack = 0;
+
+  for (let i = y; i < y + 8; i++) {
+    for (let j = x; j < x + 8; j++) {
+      // whiteBoard 비교
+      if (input[i][j] !== whiteBoard[i - y][j - x]) checkWhite++;
+
+      // blackBoard 비교
+      if (input[i][j] !== blackBoard[i - y][j - x]) checkBlack++;
+    }
+  }
+  let min = Math.min(checkWhite, checkBlack);
+  answer = Math.min(answer, min);
+}
+console.log(answer);
