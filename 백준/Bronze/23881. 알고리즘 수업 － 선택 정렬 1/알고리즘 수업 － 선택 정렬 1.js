300abb3981cecfd610de72ff780ee24fde9bd524
dali <dali1999@naver.com>
2024-02-14 16:36:57 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 260 ms, Memory: 10824 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.js
@@ -0,0 +1,30 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.split(' ').map(Number));
+
+const [, K] = input.shift();
+const arr = input[0];
+let answer = -1;
+
+function selectionSort(arr) {
+  let count = 0;
+  for (let i = arr.length - 1; i >= 0 && count < K; i--) {
+    let max = i;
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] > arr[max]) max = j;
+    }
+    if (i !== max) {
+      [arr[i], arr[max]] = [arr[max], arr[i]];
+      count++;
+      if (count === K) {
+        answer = [arr[max], arr[i]].join(' ');
+      }
+    }
+  }
+  return answer;
+}
+
+console.log(selectionSort(arr));
